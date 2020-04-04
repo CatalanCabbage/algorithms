5321ca7f1a481913418dab0b249e8c01130b349b
davis.j <davis.j@zohocorp.com>
2020-04-04 16:07:13 +0530

    Added Stacks and Queues template

---
+++ src/stacksQueues/Permutation.java
@@ -0,0 +1,16 @@
+/**
+ * @author Davis Jeffrey
+ */
+
+/**
+ * Requirement:
+ * Client class. Take an integer k as a command-line argument;
+ * read a sequence of strings from standard input using StdIn.readString();
+ * print exactly k of them, uniformly at random.
+ * Print each item from the sequence at most once.
+ */
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+}
